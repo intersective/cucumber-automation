61ca672e6d9c05dc0fea94ef7968562ff7873894
Barry Chen <barry@intersective.com>
2018-04-13 10:47:11 +1000

    add contact number field in the enrolment
    remove status field and add contact number field in the enrolment

--- features/step_definitions/appv1/testAppV1Enrolment_steps.rb
+++ features/step_definitions/appv1/testAppV1Enrolment_steps.rb
@@ -6,7 +6,7 @@ Then(/^I input the enrolment information which is located at "([^"]*)"$/) do |ar
 	studentAccount = "%s@practera.com" % [studentName]
 	$sharedData1.putData("studentName", studentName)
 	$sharedData1.putData("studentAccount", studentAccount)
-	csvtext = "%s,%s,%s,fullaccess" % [studentAccount, studentId, studentName]
+	csvtext = "%s,%s,%s,%s" % [studentAccount, studentId, studentName, Random.rand(1000000000...20000000000).to_s]
 	waitForElement($driver, $wait, arg1).send_keys(csvtext)
 end
 
