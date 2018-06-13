fab35104096dad28b025ecf6ae92894ea17c62e4
Barry Chen <barry@intersective.com>
2018-06-13 10:55:06 +1000

    add browser stack integration
    invoking remote hub url to initilise the remote driver on the browser stack.

--- module/sharedWebDriver.rb
+++ module/sharedWebDriver.rb
@@ -8,23 +8,36 @@ class SharedWebDriver
 	
 	def initialize
 		tconfigObj = readJsonfile(Dir.pwd + "/configuration/user.json")
-		Selenium::WebDriver::Chrome.driver_path=tconfigObj["driverPath"]
-		if tconfigObj["headless"]
-			if tconfigObj["server"]
-				options = Selenium::WebDriver::Chrome::Options.new
-				options.add_argument('--headless')
-				@driver = Selenium::WebDriver.for(:chrome, options: options)
-			else
-				caps = Selenium::WebDriver::Remote::Capabilities.chrome(chromeOptions: {
-					binary: "/Applications/Google\ Chrome\ Canary.app/Contents/MacOS/Google\ Chrome\ Canary", 
-					args: [ "--headless" ]})
-				@driver = Selenium::WebDriver.for(:chrome, desired_capabilities: caps)
+		if tconfigObj["browserstack"]
+			caps = Selenium::WebDriver::Remote::Capabilities.new
+			caps["os"] = "Windows"
+			caps["os_version"] = "10"
+			caps["browser"] = "Chrome"
+			caps["browser_version"] = "66.0"
+			caps["resolution"] = "1440x900"
+			caps["browserstack.local"] = "false"
+			caps["browserstack.selenium_version"] = "3.11.0"
+			remoteHunUrl = "http://%s:%s@hub-cloud.browserstack.com/wd/hub" % [tconfigObj["browserstackUsername"], tconfigObj["browserstackAccessKey"]]
+			@driver = Selenium::WebDriver.for(:remote, :url => remoteHunUrl, :desired_capabilities => caps)
+		else	
+			Selenium::WebDriver::Chrome.driver_path=tconfigObj["driverPath"]
+			if tconfigObj["headless"]
+				if tconfigObj["server"]
+					options = Selenium::WebDriver::Chrome::Options.new
+					options.add_argument('--headless')
+					@driver = Selenium::WebDriver.for(:chrome, options: options)
+				else
+					caps = Selenium::WebDriver::Remote::Capabilities.chrome(chromeOptions: {
+						binary: "/Applications/Google\ Chrome\ Canary.app/Contents/MacOS/Google\ Chrome\ Canary", 
+						args: [ "--headless" ]})
+					@driver = Selenium::WebDriver.for(:chrome, desired_capabilities: caps)
+				end
+			elsif
+				@driver = Selenium::WebDriver.for(:chrome)
 			end
-		elsif
-			@driver = Selenium::WebDriver.for(:chrome)
+			@driver.manage.window.move_to(0, 0)
+			@driver.manage.window.resize_to(1440, 1000)
 		end
-		@driver.manage.window.move_to(0, 0)
-		@driver.manage.window.resize_to(1440, 1000)
 		@wait = Selenium::WebDriver::Wait.new(:timeout => 30) # seconds
 		@listWait = Selenium::WebDriver::Wait.new(:timeout => 120) # seconds
 		@shortWait = Selenium::WebDriver::Wait.new(:timeout => 3) # seconds
