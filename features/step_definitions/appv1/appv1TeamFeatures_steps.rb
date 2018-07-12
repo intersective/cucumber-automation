44cac66d6b58b11b145a26f045b49a538a4a45ce
Barry Chen <barry@intersective.com>
2018-07-12 14:35:33 +1000

    add appv1 team page steps

---
+++ features/step_definitions/appv1/appv1TeamFeatures_steps.rb
@@ -0,0 +1,25 @@
+
+
+Then("\"Appv1 Team\" I login with team member {string} and password {string} and domain {string}") do |student, password, domain|
+    teamObj = $sharedData1.loadDataFromKey("team")
+    studentName = teamObj[student] + "@" + domain
+    step("\"Appv1\" I login with username \"#{studentName}\" and password \"#{password}\"")
+end
+
+Then("\"Appv1 Team\" I should be able to see the team name with assert") do
+    teamObj = $sharedData1.loadDataFromKey("team")
+    teamName = teamObj["name"].capitalize
+    step("I should be able to see \"team title\" which is located at \"//*[text()='#{teamName}']\" with xpath assert")
+end
+
+Then("\"Appv1 Team\" I can see {string} team member {string} name") do |memberIndex, student|
+    teamObj = $sharedData1.loadDataFromKey("team")
+    studentName = teamObj[student]
+    step("I can see \"member name\" which is located at \"[ng-repeat='member in members']:nth-of-type(#{memberIndex}) > [ng-bind='member.name']\" containing text \"#{studentName}\"")
+end
+
+Then("\"Appv1 Team\" I can see {string} team member {string} email with domain {string}") do |memberIndex, student, domain|
+    teamObj = $sharedData1.loadDataFromKey("team")
+    studentEmail = teamObj[student] + "@" + domain
+    step("I can see \"member name\" which is located at \"[ng-repeat='member in members']:nth-of-type(#{memberIndex}) > [ng-bind='member.email']\" containing text \"#{studentEmail}\"")
+end
