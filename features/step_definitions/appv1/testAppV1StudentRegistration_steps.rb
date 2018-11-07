16b3a9bd4098d35f6f208ff1e7f6d86894a9bec1
Barry Chen <barry@intersective.com>
2018-11-07 16:03:21 +1100

    move the app registration password
    move the app registration password from the feature files

--- features/step_definitions/appv1/testAppV1StudentRegistration_steps.rb
+++ features/step_definitions/appv1/testAppV1StudentRegistration_steps.rb
@@ -5,7 +5,8 @@ Then(/^I move to "([^"]*)" which is located at "([^"]*)" and click$/) do |arg1, 
 	$driver.action.move_to(ele, 0, 0).click.perform
 end
 
-Then(/^I type my own password "([^"]*)" for registration$/) do |userPassword|
+Then(/^I type my own password "([^"]*)" for registration$/) do |regPassword|
+	userPassword = $sharedData1.loadDataFromKey(regPassword)
 	waitForElement($driver, $wait, "input[name='uPassword']").send_keys(userPassword)
 	waitForElement($driver, $wait, "input[name='uVerifyPassword']").send_keys(userPassword)
 end
