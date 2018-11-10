47ebce612d301c75984516d5f0b5c364dbe478d7
Barry Chen <barry@intersective.com>
2018-11-10 19:20:20 +1100

    Update copyreport.sh

--- copyreport.sh
+++ copyreport.sh
@@ -3,4 +3,4 @@ NOW=$(date -u +'%S-%M-%H-%d-%m-%Y')
 TESTSUITNAME=$1
 S3REPORTNAME=$(awk '{split($0, a, "."); print a[1]}' <<< $TESTSUITNAME)
 ruby tools/test_reports.rb > reports/report-date.html
-aws s3 cp reports/report-date.html s3://sydney-test-data/ui-automation/reports/$S3REPORTNAME-$NOW.html
+aws s3 cp reports/report-date.html s3://$2/$S3REPORTNAME-$NOW.html
