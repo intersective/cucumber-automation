9af0833f57669cb5463eae627943d1683f9d1acf
Barry Chen <barry@intersective.com>
2018-11-10 21:19:25 +1100

    Update run.sh
    make run script to handle a single feature file and guard any invalid file extension

--- run.sh
+++ run.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
+
 touch logs/error.log
 touch logs/info.log
 touch logs/trace.log
 touch logs/testcases.log
 reportFile="reports/report.html"
 featuresFile=$1
-if [ -f "$reportFile" ]
+fileextension=${featuresFile#*.}
+
+if [ $fileextension = "txt" -o $fileextension = "feature" ]
 then
-	now=$(date -u +'%S-%M-%H-%d-%m-%Y')
-	cp "$reportFile" "$reportFile.$now"
+	if [ -f "$reportFile" ]
+	then
+		now=$(date -u +'%S-%M-%H-%d-%m-%Y')
+		cp "$reportFile" "$reportFile.$now"
+	fi
+	if [ $fileextension = "txt" ]
+	then
+		cucumber -q @"$featuresFile" --format html --out "$reportFile" --format pretty
+	else
+		cucumber -q $featuresFile --format html --out reports/report.html --format pretty
+	fi
+else
+	echo $fileextension
 fi
-cucumber -q @"$featuresFile" --format html --out "$reportFile" --format pretty
