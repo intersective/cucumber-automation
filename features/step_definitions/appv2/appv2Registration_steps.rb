eedbfe291fcff7393353cc075679194b99631430
Barry Chen <barry@intersective.com>
2019-01-22 13:23:06 +1100

    add click action to the ion-input elements

--- features/step_definitions/appv2/appv2Registration_steps.rb
+++ features/step_definitions/appv2/appv2Registration_steps.rb
@@ -2,6 +2,10 @@
 
 Then(/^"Appv2" I type my own password "([^"]*)" for registration$/) do |regPassword|
 	userPassword = loadSharedData(regPassword, regPassword)
-	waitForElement($driver, $wait, "ion-input[formcontrolname='password']").send_keys(userPassword)
-	waitForElement($driver, $wait, "ion-input[formcontrolname='confirmPassword']").send_keys(userPassword)
+	passworde = waitForElement($driver, $wait, "ion-input[formcontrolname='password']")
+	passworde.click()
+	passworde.send_keys(userPassword)
+	confirmPassworde = waitForElement($driver, $wait, "ion-input[formcontrolname='confirmPassword']")
+	confirmPassworde.click()
+	confirmPassworde.send_keys(userPassword)
 end
