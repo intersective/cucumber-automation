90230e38501abaac2bd326dd0300d1429b7bdc54
Barry Chen <barry@intersective.com>
2019-11-21 16:19:52 +1100

    event check in test cases

--- features/step_definitions/appv2/appv2Events_steps.rb
+++ features/step_definitions/appv2/appv2Events_steps.rb
@@ -41,8 +41,7 @@ end
 
 Then("\"Appv2 events\" I book an event {string}") do |eventName|
     itemLocator = "//app-events//event-card//*[@class='content']/*[1][normalize-space()='#{eventName}']"
-    ele = wait_for_element_xpath($driver, $wait, itemLocator)
-    ele.click()
+    ele = wait_for_element_xpath($driver, $wait, itemLocator).click()
     sleep 2
     wait_for_element_xpath($driver, $wait, "//ion-button[normalize-space()='Book']").click()
     wait_for_element_xpath($driver, $wait, "//ion-alert//div[normalize-space()='Booked Successfully!']")
@@ -58,4 +57,23 @@ end
 
 Then("\"Appv2 events\" I go back") do
     wait_for_element_xpath($driver, $wait, "//app-events//ion-icon[@name='arrow-back']").click()
+end
+
+Then("\"Appv2 events\" I do the checkin assessment of the event") do
+    eventName = load_shared_data(Application.KEY_CURRENTEVENT)["1"].name
+    step("\"Appv2 events\" I do the checkin assessment of an event \"#{eventName}\"")
+end
+
+Then("\"Appv2 events\" I do the checkin assessment of an event {string}") do |eventName|
+    itemLocator = "//app-events//event-card//*[@class='content']/*[1][normalize-space()='#{eventName}']"
+    ele = wait_for_element_xpath($driver, $wait, itemLocator).click()
+    wait_for_element_xpath($driver, $wait, "//ion-button[normalize-space()='Check In']").click()
+
+    step("\"Appv2\" I answer \"New Choice 1\" for question 1 with question type \"multiple choice\"")
+    step("\"Appv2\" I answer \"ohhh gogoggoo\" for question 2 with question type \"text\"")
+    step("\"Appv2\" I answer \"New Choice 1,New Choice 0\" for question 3 with question type \"checkbox\"")
+    step("\"Appv2\" I submit the assessment")
+    while wait_for_element_xpath($driver, $wait, "//app-assessment") != nil
+        sleep 1
+    end
 end
